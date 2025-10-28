45c604d32e703401b0fab765723497311132a895
Abdullah Gumi <abdulgumi77@gmail.com>
2025-10-28 19:38:22 +0100

    fix

--- api/client/apiClient.ts
+++ api/client/apiClient.ts
@@ -5,8 +5,8 @@ import axios from "axios";
 import { useAppStore } from "@/stores/useAppStore";
 
 const apiClient = axios.create({
-  baseURL: "http://172.20.10.2:3000/api",
-  // baseURL: "https://api.betterkaduna.com/api",
+  // baseURL: "http://172.20.10.2:3000/api",
+  baseURL: "https://api.betterkaduna.com/api",
   headers: {
     "Content-Type": "application/json",
     Accept: "application/json",
